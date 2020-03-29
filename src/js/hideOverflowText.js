0247f904b079f1b0cabadbca8196fadfcb42da43
YC-Yuan <YC-Yuan@users.noreply.github.com>
2020-03-29 22:56:27 +0800

    -3/29 js优化溢出文本省略√
    优化主页浮动比例
    图片裁剪&移动适应

---
+++ src/js/hideOverflowText.js
@@ -0,0 +1,21 @@
+//使用说明：将需要省略的文本设定为"textToHide"class,并嵌套在设定"textDiv"class的div中
+function changeLine() {
+    var div = document.getElementsByClassName('textDiv');
+    //alert(1);
+    var p = document.getElementsByClassName('textToHide');
+    //alert('length:' + div.length);
+    for (var i = 0; i < div.length; i++) {
+        //alert(2);
+        var height = parseInt(window.getComputedStyle(div[i]).height);
+        //alert(3);
+        var padding = parseInt(window.getComputedStyle(div[i]).padding);
+        //alert(4);
+        var fontSize = parseInt(window.getComputedStyle(p[i]).fontSize);
+        //alert(5);
+        var margin = parseInt(window.getComputedStyle(p[i]).margin);
+        //alert(6);
+        var line = Math.floor((height - 2 * (padding + margin)) / fontSize);
+        p[i].style.webkitLineClamp = line.toString();
+    }
+    //alert(line);
+}
